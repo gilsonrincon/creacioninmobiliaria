cf8f50fd23bd3a406a59ec96b17a7282bfd2f05e
Lighting snake <andres.territoriovirtual@gmail.com>
2014-03-12 09:31:56 -0500

    Pop-up
    Se ha agregado un div y el codigo para mostrarlo cuando se de a
    contactar en el formulario de contacto del producto. Falta aplicar los
    estilos.

--- modules/productcontact/productcontact.js
+++ modules/productcontact/productcontact.js
@@ -8,7 +8,18 @@ $("#send").click(function(event) {
 	
 	$.post('http://creacioninmobiliaria.com/libraries/ajax.php', {nombre: $nombre, telefono: $telefono, email: $email, comentarios: $comentarios}, function(data) {
 		/* Eventos que ocurriran al enviarse un mensaje */
+		/*
 		alert(data);
+		*/
+		$('#popup-msg').text(data);
+		$('#popup').fadeIn(1000);
+	});
+
+	$('#popup .close').click(function(event) {
+		/* Act on the event */
+		$('#popup').fadeOut('1000');
+		event.preventDefault();
+		return false;
 	});
 
 	event.preventDefault()
--- modules/productcontact/productcontact.tpl
+++ modules/productcontact/productcontact.tpl
@@ -14,4 +14,9 @@ Este modulo muestra las propiedades con categoria de destacadas.
 	</form>
 	<img class="logo-empresa" src="img/creacion/img-contact-product.png" />
 </div>
+<div id="popup">
+	<a href="#" class="close">Cerrar</a>
+	<div id="popup-msg">
+	</div>
+</div>
 <script type="text/javascript" src="http://creacioninmobiliaria.com/modules/productcontact/productcontact.js"></script>
